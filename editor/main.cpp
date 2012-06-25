f581327e54b7e0a42d752fd552b8311b0c7c2765
lucas8 <luc.linux@mailoo.org>
2012-06-25 09:47:28 +0200

    Récupération du chemin.

--- editor/main.cpp
+++ editor/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <boost/filesystem/path.hpp>
 
 int main(int argc, char *argv[])
 {
-	if(argc && argv) {} // Avoid warnings
+	if(argc < 2)
+	{
+		std::cerr<<"Absence d'argument."<<std::endl;
+		return 1;
+	}
+
+	boost::filesystem::path path(argv[1]);
+	std::cout<<path.string()<<std::endl;
 
 	return 0;
 }

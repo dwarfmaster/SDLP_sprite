e397fbe4f2570ffec0b0cd8a992d7d663f296b2d
lucas8 <luc.linux@mailoo.org>
2012-06-28 09:41:01 +0200

    Correction d'un bug au scrolling.

--- editor/editorArea.cpp
+++ editor/editorArea.cpp
@@ -11,52 +11,56 @@
 
 void EditorArea::mouseClicked(gcn::MouseEvent& event)
 {
-	sdl::Pointsi ori(getHorizontalScrollAmount(), getVerticalScrollAmount());
-	sdl::Pointsi pos(event.getX() + ori.x, event.getY() + ori.y);
-
-	sdl::Pointsi pos2;
-	sdl::Pointsi topleft;
-	sdl::Pointsi bottomright;
-	sdl::AABB aabb;
-
-	switch(event.getButton())
+	if( event.getX() < getWidth() - getScrollbarWidth()
+			&& event.getY() < getHeight() - getScrollbarWidth() )
 	{
-		case gcn::MouseEvent::LEFT: // Set Bottom Left
-			pos2 = m_editor->get().location(sdl::AABB::BOTTOM_RIGHT);
-			topleft = sdl::min(pos, pos2);
-			bottomright = sdl::max(pos, pos2);
+		sdl::Pointsi ori(getHorizontalScrollAmount(), getVerticalScrollAmount());
+		sdl::Pointsi pos(event.getX() + ori.x, event.getY() + ori.y);
 
-			if(bottomright.x - topleft.x < 4)
-				bottomright.x = topleft.x + 4;
-			if(bottomright.y - topleft.y < 4)
-				bottomright.y = topleft.y + 4;
+		sdl::Pointsi pos2;
+		sdl::Pointsi topleft;
+		sdl::Pointsi bottomright;
+		sdl::AABB aabb;
 
-			aabb.set(topleft, bottomright);
-			m_editor->modifie(aabb);
-			break;
+		switch(event.getButton())
+		{
+			case gcn::MouseEvent::LEFT: // Set Bottom Left
+				pos2 = m_editor->get().location(sdl::AABB::BOTTOM_RIGHT);
+				topleft = sdl::min(pos, pos2);
+				bottomright = sdl::max(pos, pos2);
 
-		case gcn::MouseEvent::RIGHT: // Set Bottom Right
-			pos2 = m_editor->get().location(sdl::AABB::TOP_LEFT);
-			topleft = sdl::min(pos, pos2);
-			bottomright = sdl::max(pos, pos2);
+				if(bottomright.x - topleft.x < 4)
+					bottomright.x = topleft.x + 4;
+				if(bottomright.y - topleft.y < 4)
+					bottomright.y = topleft.y + 4;
 
-			if(bottomright.x - topleft.x < 4)
-				bottomright.x = topleft.x + 4;
-			if(bottomright.y - topleft.y < 4)
-				bottomright.y = topleft.y + 4;
+				aabb.set(topleft, bottomright);
+				m_editor->modifie(aabb);
+				break;
 
-			aabb.set(topleft, bottomright);
-			m_editor->modifie(aabb);
-			break;
+			case gcn::MouseEvent::RIGHT: // Set Bottom Right
+				pos2 = m_editor->get().location(sdl::AABB::TOP_LEFT);
+				topleft = sdl::min(pos, pos2);
+				bottomright = sdl::max(pos, pos2);
 
-		case gcn::MouseEvent::MIDDLE: // Set hotpoint
-			m_editor->setHotPoint(pos);
-			break;
+				if(bottomright.x - topleft.x < 4)
+					bottomright.x = topleft.x + 4;
+				if(bottomright.y - topleft.y < 4)
+					bottomright.y = topleft.y + 4;
 
-		default: // Avoid warnings
-			break;
+				aabb.set(topleft, bottomright);
+				m_editor->modifie(aabb);
+				break;
+
+			case gcn::MouseEvent::MIDDLE: // Set hotpoint
+				m_editor->setHotPoint(pos);
+				break;
+
+			default: // Avoid warnings
+				break;
+		}
+		m_fen->update();
 	}
-	m_fen->update();
 
 	ScrollArea::mouseClicked(event);
 }

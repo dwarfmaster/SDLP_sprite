ed34e91f6964e2b16672b64a45b29684f2f248c5
lucas8 <luc.linux@mailoo.org>
2012-07-22 11:53:45 +0200

    Modification de la syntaxe xml du sprite et ajout d'une nouvelle classe pour le prendre en charge.

---
+++ spritefile.cpp
@@ -0,0 +1,6 @@
+#include "spritefile.hpp"
+
+namespace sdl
+{
+};
+
---
+++ spritefile.hpp
@@ -0,0 +1,15 @@
+#ifndef DEF_SDLP_SPRITEFILE
+#define DEF_SDLP_SPRITEFILE
+
+#include "asprite.hpp"
+
+namespace sdl
+{
+	class SpriteFile
+	{
+		public:
+		private:
+	};
+};
+
+#endif//DEF_SDLP_SPRITEFILE

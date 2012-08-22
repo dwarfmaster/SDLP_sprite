70eba73e3c549cf4be2479ac18b5adbf5608f28f
lucas8 <luc.linux@mailoo.org>
2012-08-22 23:59:41 +0200

    Correction d'erreurs de déclarations de sous structures: GAABB et sprite

--- src/asprite.hpp
+++ src/asprite.hpp
@@ -50,61 +50,60 @@ namespace sdl
 
 	class ASprite
 	{
-		struct GAABB;
+		struct GAABB // Groupe de SAABB
+		{
+			AABB global; /*!< AABB englobant les sous AABBs. */
+			std::vector<AABB> aabbs; /*!< Les sous AABBs. */
+			unsigned int priority; /*!< La priorité, telle que 0 soit le niveau maximum. */
+		};
 
 		public:
-			typedef boost::filesystem::path path_t;
-			friend SpriteEditor;
+		typedef boost::filesystem::path path_t;
+		friend SpriteEditor;
 
-			ASprite();
-			ASprite(const ASprite& cp);
-			ASprite(const std::map<std::string, gaabb>& groups, const Pointsi& hotp, const boost::shared_ptr<SDL_Surface>& img);
+		ASprite();
+		ASprite(const ASprite& cp);
+		ASprite(const std::map<std::string, gaabb>& groups, const Pointsi& hotp, const boost::shared_ptr<SDL_Surface>& img);
 
-			void set(const ASprite& cp);
-			void set(const std::map<std::string, gaabb>& groups, const Pointsi& hotp, const boost::shared_ptr<SDL_Surface>& img);
-			void clear();
+		void set(const ASprite& cp);
+		void set(const std::map<std::string, gaabb>& groups, const Pointsi& hotp, const boost::shared_ptr<SDL_Surface>& img);
+		void clear();
 
-			ASprite& operator=(const ASprite& cp);
-			operator SDL_Surface() const;
-			operator boost::shared_ptr<SDL_Surface>();
+		ASprite& operator=(const ASprite& cp);
+		operator SDL_Surface() const;
+		operator boost::shared_ptr<SDL_Surface>();
 
-			boost::shared_ptr<SDL_Surface> get() const;
-			SDL_Surface* getPtr() const; // Dangereux
+		boost::shared_ptr<SDL_Surface> get() const;
+		SDL_Surface* getPtr() const; // Dangereux
 
-			Pointsi hotPoint() const;
-			Vector2f decale() const; // retourne le décalage nécessaire à partir du hotPoint.
+		Pointsi hotPoint() const;
+		Vector2f decale() const; // retourne le décalage nécessaire à partir du hotPoint.
 
-			AABB global() const;
-			std::vector<AABB> saabb(std::string id) const;
-			AABB globalSAABB(std::string id) const;
-			std::vector<AABB> operator[](std::string id) const;
-			bool exist(std::string id) const;
-			std::vector<std::string> groups() const;
-			std::vector<std::string> inGroup(const Pointsi& p) const;
-			std::string inUniqueGroup(const Pointsi& p) const;
-			unsigned int priority(std::string id) const;
+		AABB global() const;
+		std::vector<AABB> saabb(std::string id) const;
+		AABB globalSAABB(std::string id) const;
+		std::vector<AABB> operator[](std::string id) const;
+		bool exist(std::string id) const;
+		std::vector<std::string> groups() const;
+		std::vector<std::string> inGroup(const Pointsi& p) const;
+		std::string inUniqueGroup(const Pointsi& p) const;
+		unsigned int priority(std::string id) const;
 
-			// Les groupes du premiers qui sont en colision avec le second
-			std::vector< std::pair<std::string, std::string> > colision(Pointsi pos1, const ASprite& other, Pointsi pos2) const;
+		// Les groupes du premiers qui sont en colision avec le second
+		std::vector< std::pair<std::string, std::string> > colision(Pointsi pos1, const ASprite& other, Pointsi pos2) const;
 
 		protected:
-			struct GAABB // Groupe de SAABB
-			{
-				AABB global; /*!< AABB englobant les sous AABBs. */
-				std::vector<AABB> aabbs; /*!< Les sous AABBs. */
-				unsigned int priority; /*!< La priorité, telle que 0 soit le niveau maximum. */
-			};
-			std::map<std::string, GAABB> m_groups; /*!< Les groupes d'AABBs. */
-			typedef std::map<std::string,GAABB>::iterator group_iterator;
-			typedef std::map<std::string,GAABB>::const_iterator c_group_iterator;
+		std::map<std::string, GAABB> m_groups; /*!< Les groupes d'AABBs. */
+		typedef std::map<std::string,GAABB>::iterator group_iterator;
+		typedef std::map<std::string,GAABB>::const_iterator c_group_iterator;
 
-			AABB m_global; /*!< L'AABB englobant tous les groupes. */
+		AABB m_global; /*!< L'AABB englobant tous les groupes. */
 
-			Pointsi m_hotPoint; /*!< Le point chaud (de blit) du sprite. */
+		Pointsi m_hotPoint; /*!< Le point chaud (de blit) du sprite. */
 
-			boost::shared_ptr<SDL_Surface> m_img; /*!< L'image à blitter. */
+		boost::shared_ptr<SDL_Surface> m_img; /*!< L'image à blitter. */
 
-			GAABB fromGaabb(const gaabb& cp);
+		GAABB fromGaabb(const gaabb& cp);
 	};
 };
 
--- src/spritefile.hpp
+++ src/spritefile.hpp
@@ -45,57 +45,56 @@ namespace sdl
 {
 	class SpriteFile
 	{
-		struct sprite;
+		struct sprite
+		{
+			AABB rect; /*!< La partie de l'image à utiliser. */
+			Pointsi hotp; /*!< Le point chaud. */
+
+			std::map<std::string, gaabb> gaabbs; /*!< Le groupes de sous AABBs. */
+		};
 
 		public:
-			typedef boost::filesystem::path path_t;
-			typedef std::map<std::string, sprite>::iterator sprite_iterator;
-			typedef std::map<std::string, sprite>::const_iterator csprite_iterator;
+		typedef boost::filesystem::path path_t;
+		typedef std::map<std::string, sprite>::iterator sprite_iterator;
+		typedef std::map<std::string, sprite>::const_iterator csprite_iterator;
 
-			SpriteFile();
-			SpriteFile(const path_t& path);
-			SpriteFile(const SpriteFile& cp);
+		SpriteFile();
+		SpriteFile(const path_t& path);
+		SpriteFile(const SpriteFile& cp);
 
-			void clear();
-			bool load(const path_t& path);
-			void set(const SpriteFile& cp);
-			SpriteFile& operator=(const SpriteFile& cp);
+		void clear();
+		bool load(const path_t& path);
+		void set(const SpriteFile& cp);
+		SpriteFile& operator=(const SpriteFile& cp);
 
-			ASprite getSprite(std::string id);
-			AABB getRect(std::string id) const;
-			ASprite operator[](std::string id);
-			bool exist(std::string id) const;
+		ASprite getSprite(std::string id);
+		AABB getRect(std::string id) const;
+		ASprite operator[](std::string id);
+		bool exist(std::string id) const;
 
-			std::vector<std::string> listIds() const;
+		std::vector<std::string> listIds() const;
 
-			bool changeSprite(std::string id, const std::map<std::string, gaabb>& groups, const Pointsi& hotp, const AABB& rect);
-			bool changeSprite(std::string id, const std::map<std::string, gaabb>& groups, const Pointsi& hotp); // Utilise le rect enregistré
-			bool addSprite(std::string id, const std::map<std::string, gaabb>& groups, const Pointsi& hotp, const AABB& rect);
-			bool deleteSprite(std::string id);
-			bool save(path_t path) const;
-			bool save() const; // Sauvegarde avec le chemin enregistré
+		bool changeSprite(std::string id, const std::map<std::string, gaabb>& groups, const Pointsi& hotp, const AABB& rect);
+		bool changeSprite(std::string id, const std::map<std::string, gaabb>& groups, const Pointsi& hotp); // Utilise le rect enregistré
+		bool addSprite(std::string id, const std::map<std::string, gaabb>& groups, const Pointsi& hotp, const AABB& rect);
+		bool deleteSprite(std::string id);
+		bool save(path_t path) const;
+		bool save() const; // Sauvegarde avec le chemin enregistré
 
-			static std::string xmlSprite(const ASprite& sprite, const std::string& id, const AABB& rect, const std::string& toadd="", const std::string& begin="");
-			static ASprite* spriteXml(TiXmlElement* sprite, const path_t& img, AABB* rect = NULL);
-			static ASprite* spriteXml(TiXmlElement* sprite, SDL_Surface* img, AABB* arect = NULL); // La surface ne sera pas modifiée
+		static std::string xmlSprite(const ASprite& sprite, const std::string& id, const AABB& rect, const std::string& toadd="", const std::string& begin="");
+		static ASprite* spriteXml(TiXmlElement* sprite, const path_t& img, AABB* rect = NULL);
+		static ASprite* spriteXml(TiXmlElement* sprite, SDL_Surface* img, AABB* arect = NULL); // La surface ne sera pas modifiée
 
 		protected:
-			path_t m_path; /*!< Le chemin du fichier chargé. */
-			path_t m_imgPath; /*!< Le chemin de l'image. */
-			boost::shared_ptr<SDL_Surface> m_img; /*!< L'image utilisée. */
+		path_t m_path; /*!< Le chemin du fichier chargé. */
+		path_t m_imgPath; /*!< Le chemin de l'image. */
+		boost::shared_ptr<SDL_Surface> m_img; /*!< L'image utilisée. */
 
-			struct sprite
-			{
-				AABB rect; /*!< La partie de l'image à utiliser. */
-				Pointsi hotp; /*!< Le point chaud. */
+		std::map<std::string, sprite> m_sprites; /*!< Le sprites. */
 
-				std::map<std::string, gaabb> gaabbs; /*!< Le groupes de sous AABBs. */
-			};
-			std::map<std::string, sprite> m_sprites; /*!< Le sprites. */
-
-			static bool parseSprite(TiXmlElement* spriten, sprite* p, const SDL_Surface* img); // Ces trois fonctions peuvent être utlisées par spriteXml
-			static void parseRect(TiXmlElement* spriten, AABB* p, const SDL_Surface* img);
-			static bool parseGAABB(TiXmlElement* gaabbn, gaabb* p);
+		static bool parseSprite(TiXmlElement* spriten, sprite* p, const SDL_Surface* img); // Ces trois fonctions peuvent être utlisées par spriteXml
+		static void parseRect(TiXmlElement* spriten, AABB* p, const SDL_Surface* img);
+		static bool parseGAABB(TiXmlElement* gaabbn, gaabb* p);
 	};
 };
 
